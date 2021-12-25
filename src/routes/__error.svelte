19989108a8933ae3dfc3ba649a4b1199597eeb38
VincentJonathan <vinjongm@gmail.com>
2021-12-25 18:52:15 +0700

    add translation for error page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,8 +1,12 @@
+<script lang="ts">
+	import { _ } from "svelte-i18n";
+</script>
+
 <div class="h-screen w-screen fixed flex flex-col justify-center">
 	<div class="max-w-5xl space-y-8">
-		<div class="font-brand text-6xl">Seems like you are lost</div>
-		<div class="text-left text-2xl">
-			Maybe you want to <a class="underline underline-offset-8" href="/">return to home</a>
+		<div class="font-brand text-6xl">{$_("error.title")}</div>
+		<div class="text-2xl">
+			<a class="underline underline-offset-8" href="/">{$_("error.subtitle")}</a>
 		</div>
 	</div>
 </div>
