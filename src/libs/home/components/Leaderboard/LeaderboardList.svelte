9ac50dd3e670150730db8890bf0d7139ead79c6e
VincentJonathan <vinjongm@gmail.com>
2021-12-26 14:18:55 +0700

    remove unnecessary div

--- src/libs/home/components/Leaderboard/LeaderboardList.svelte
+++ src/libs/home/components/Leaderboard/LeaderboardList.svelte
@@ -11,19 +11,17 @@
 	$: href = `${isPlayer ? "p" : "t"}/${item.name.replace("#", "-")}`;
 </script>
 
-<div class="rounded bg-white bg-opacity-5 px-2 py-1" in:fade={{ duration: 150 }}>
-	<a {href}>
-		<div class="flex flex-row items-center space-x-4">
-			<Text variant="title4" class="font-brand text-shadow w-6">{position}.</Text>
+<a {href} class="rounded bg-white bg-opacity-5 px-2 py-1" in:fade={{ duration: 150 }}>
+	<div class="flex flex-row items-center space-x-4">
+		<Text variant="title4" class="font-brand text-shadow w-6">{position}.</Text>
 
-			<div class="flex flex-col truncate">
-				<Text variant="body1" class="font-semibold truncate">
-					{item.name}
-				</Text>
-				<Text variant="caption">
-					{$_("home.score")}: {item.score}
-				</Text>
-			</div>
+		<div class="flex flex-col truncate">
+			<Text variant="body1" class="font-semibold truncate">
+				{item.name}
+			</Text>
+			<Text variant="caption">
+				{$_("home.score")}: {item.score}
+			</Text>
 		</div>
-	</a>
-</div>
+	</div>
+</a>
