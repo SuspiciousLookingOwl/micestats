33b05d9afb28d89306b833e94e8e6c01d695cd6b
VincentJonathan <vinjongm@gmail.com>
2021-12-28 12:11:39 +0700

    add tribe

--- src/libs/profile/components/ProfileOverview/SideProfileOverview.svelte
+++ src/libs/profile/components/ProfileOverview/SideProfileOverview.svelte
@@ -26,7 +26,7 @@
 		{!isInView && 'lg:fixed lg:top-0 lg:mt-2 !ml-0'}"
 >
 	<Card class="main-container">
-		<div class="flex flex-col items-center space-y-4 p-4">
+		<div class="flex flex-col items-center p-4">
 			<!-- name -->
 			{#if !isInView}
 				<div in:fly={{ y: -32 }} class="hidden lg:block">
@@ -44,23 +44,33 @@
 			</Carousel>
 
 			<!-- title -->
-			<AsyncLoader promise={profile.getTitle($locale || undefined)} size="sm" let:value>
-				<Text class="italic">
-					« {value} »
-				</Text>
-			</AsyncLoader>
+			<div class="mt-4 flex flex-col items-center space-y-2">
+				<AsyncLoader promise={profile.getTitle($locale || undefined)} size="sm" let:value>
+					<Text class="italic">
+						« {value} »
+					</Text>
+				</AsyncLoader>
 
-			<!-- soulmate -->
-			{#if profile.soulmate}
-				<div class="pt-4">
+				<!-- soulmate -->
+				{#if profile.soulmate}
 					<a href="/p/{profile.soulmate.slugName}" class="flex items-center space-x-2">
 						<Icon name="heart" class="fill-white mt-1" />
 						<Text>
 							{profile.soulmate.name}
 						</Text>
 					</a>
-				</div>
-			{/if}
+				{/if}
+
+				<!-- tribe -->
+				{#if profile.tribe}
+					<div class="flex items-center space-x-2">
+						<Icon name="home" class="fill-white mt-1" />
+						<Text>
+							{profile.tribe.name}
+						</Text>
+					</div>
+				{/if}
+			</div>
 		</div>
 	</Card>
 </div>
