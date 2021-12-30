b25f82af38b172fcc487c4bc9a37b5c15d5c21e2
VincentJonathan <vinjongm@gmail.com>
2021-12-30 19:24:16 +0700

    add index file

--- src/routes/leaderboards/index.svelte
+++ src/routes/leaderboards/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { background } from "@libs/app";
-	import Main from "@libs/leaderboard/views/Main.svelte";
+	import { Main } from "@libs/leaderboard";
 
 	background.setXOffset(0.15);
 </script>
