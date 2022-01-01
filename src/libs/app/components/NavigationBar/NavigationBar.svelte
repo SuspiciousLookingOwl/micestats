c15e163be16844d9b06c46f0c56d974806a1bef0
VincentJonathan <vinjongm@gmail.com>
2022-01-01 12:36:06 +0700

    allow prefix path to be highlighted

--- src/libs/app/components/NavigationBar/NavigationBar.svelte
+++ src/libs/app/components/NavigationBar/NavigationBar.svelte
@@ -11,6 +11,7 @@
 		{
 			name: $_("nav.leaderboards"),
 			path: "/leaderboards",
+			prefix: true,
 		},
 		{
 			name: $_("nav.search"),
@@ -21,18 +22,24 @@
 			path: "/about",
 		},
 	];
+
+	const isActivePath = (currentPath: string, path: string, prefix = false) => {
+		return path === currentPath || (prefix && currentPath.startsWith(path));
+	};
 </script>
 
 <div class="navbar">
-	{#each routes as { name, path }}
+	{#each routes as { name, path, prefix }}
 		<a href={path} class="cursor-pointer hover:text-shadow hover:transition-all px-4 py-2">
 			<Text
 				variant="subtitle1"
-				class={path === $page.path ? "font-medium text-white text-shadow" : ""}
+				class={isActivePath($page.path, path, prefix)
+					? "font-medium text-white text-shadow"
+					: ""}
 			>
 				{name}
 			</Text>
-			{#if path === $page.path}
+			{#if isActivePath($page.path, path, prefix)}
 				<div class="top-[0.625rem] relative border-b-2 border-white box-border" />
 			{/if}
 		</a>
@@ -40,8 +47,9 @@
 
 	<div class="flex-grow text-right px-4">
 		<a href="/">
-			<Text variant="title2" class="text-shadow-md font-brand text-center">Cheeseformice</Text
-			>
+			<Text variant="title2" class="text-shadow-md font-brand text-center">
+				Cheeseformice
+			</Text>
 		</a>
 	</div>
 </div>
