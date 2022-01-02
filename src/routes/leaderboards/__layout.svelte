7dac8e60ed93b92f8770b4d7af6e66fe480a8dbd
VincentJonathan <vinjongm@gmail.com>
2022-01-02 09:26:30 +0700

    add title

--- src/routes/leaderboards/__layout.svelte
+++ src/routes/leaderboards/__layout.svelte
@@ -1,6 +1,7 @@
 <script context="module" lang="ts">
 	import { TransitionedRoutes } from "@components";
 	import type { Load } from "@sveltejs/kit";
+	import { _ } from "svelte-i18n";
 
 	export const load: Load = async ({ page }) => {
 		return { props: { path: page.path } };
@@ -10,6 +11,10 @@
 <script lang="ts">
 	export let path: string;
 </script>
+
+<svelte:head>
+	<title>{$_("home.leaderboards")} | Cheeseformice</title>
+</svelte:head>
 
 <TransitionedRoutes level={2} deep {path}>
 	<slot />
--- src/routes/search.svelte
+++ src/routes/search.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { background, SearchInput } from "@libs/app";
 	import { onMount } from "svelte";
+	import { _ } from "svelte-i18n";
 
 	let isFocused = false;
 
@@ -11,6 +12,10 @@
 		setTimeout(() => (isFocused = true), 500);
 	});
 </script>
+
+<svelte:head>
+	<title>{$_("nav.search")} | Cheeseformice</title>
+</svelte:head>
 
 <div class="w-full">
 	<SearchInput {isFocused} />
