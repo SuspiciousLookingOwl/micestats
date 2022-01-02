62ae6ac212cfe6d1fd1056bf09c5227ddccf514c
VincentJonathan <vinjongm@gmail.com>
2022-01-02 09:41:04 +0700

    make navbar reusable

--- src/libs/app/components/NavigationBar/NavigationBar.svelte
+++ src/libs/app/components/NavigationBar/NavigationBar.svelte
@@ -1,56 +1,41 @@
+<script lang="ts" context="module">
+	export interface Route {
+		name: string;
+		path: string;
+		exact?: boolean;
+	}
+</script>
+
 <script lang="ts">
 	import { page } from "$app/stores";
 	import { Text } from "@components";
-	import { _ } from "svelte-i18n";
 
-	const routes = [
-		{
-			name: $_("nav.home"),
-			path: "/",
-		},
-		{
-			name: $_("nav.leaderboards"),
-			path: "/leaderboards",
-			prefix: true,
-		},
-		{
-			name: $_("nav.search"),
-			path: "/search",
-		},
-		{
-			name: $_("nav.about"),
-			path: "/about",
-		},
-	];
+	export let routes: Route[] = [];
 
-	const isActivePath = (currentPath: string, path: string, prefix = false) => {
-		return path === currentPath || (prefix && currentPath.startsWith(path));
+	const isActivePath = (currentPath: string, path: string, exact = false) => {
+		return path === currentPath || (!exact && currentPath.startsWith(path));
 	};
 </script>
 
 <div class="navbar">
-	{#each routes as { name, path, prefix }}
+	{#each routes as { name, path, exact }}
 		<a href={path} class="cursor-pointer hover:text-shadow hover:transition-all px-4 py-2">
 			<Text
 				variant="subtitle1"
-				class={isActivePath($page.path, path, prefix)
+				class={isActivePath($page.path, path, exact)
 					? "font-medium text-white text-shadow"
 					: ""}
 			>
 				{name}
 			</Text>
-			{#if isActivePath($page.path, path, prefix)}
+			{#if isActivePath($page.path, path, exact)}
 				<div class="top-[0.625rem] relative border-b-2 border-white box-border" />
 			{/if}
 		</a>
 	{/each}
 
 	<div class="flex-grow text-right px-4">
-		<a href="/">
-			<Text variant="title2" class="text-shadow-md font-brand text-center">
-				Cheeseformice
-			</Text>
-		</a>
+		<slot name="right" />
 	</div>
 </div>
 
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts" context="module">
 	import { browser } from "$app/env";
-	import { TransitionedRoutes } from "@components";
+	import { Text, TransitionedRoutes } from "@components";
 	import { Background, BottomNavigator } from "@libs/app";
 	import NavigationBar from "@libs/app/components/NavigationBar/NavigationBar.svelte";
 	import type { Load } from "@sveltejs/kit";
-	import { addMessages, getLocaleFromQueryString, init } from "svelte-i18n";
+	import { addMessages, getLocaleFromQueryString, init, _ } from "svelte-i18n";
 	import "../app.css";
 	import locales from "../locales";
 
@@ -32,6 +32,26 @@
 
 <script lang="ts">
 	export let path: string;
+
+	const routes = [
+		{
+			name: $_("nav.home"),
+			path: "/",
+			exact: true,
+		},
+		{
+			name: $_("nav.leaderboards"),
+			path: "/leaderboards",
+		},
+		{
+			name: $_("nav.search"),
+			path: "/search",
+		},
+		{
+			name: $_("nav.about"),
+			path: "/about",
+		},
+	];
 </script>
 
 <svelte:head>
@@ -39,14 +59,23 @@
 </svelte:head>
 
 <Background />
+
 <div class="w-full max-w-7xl mx-auto px-2 md:px-4 py-4 md:space-y-2 xl:space-y-4 2xl:space-y-8">
 	<div class="hidden lg:block">
-		<NavigationBar />
+		<NavigationBar {routes}>
+			<a href="/" slot="right">
+				<Text variant="title2" class="text-shadow-md font-brand text-center">
+					Cheeseformice
+				</Text>
+			</a>
+		</NavigationBar>
 	</div>
+
 	<TransitionedRoutes {path}>
 		<slot />
 	</TransitionedRoutes>
 </div>
+
 <div class="lg:hidden">
 	<BottomNavigator />
 </div>
