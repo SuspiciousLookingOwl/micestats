af98c9d4035ad2a21f732dbd3a3e5bb8053062ca
VincentJonathan <vinjongm@gmail.com>
2022-01-04 17:50:22 +0700

    move leaderboard to index

---
+++ src/routes/leaderboards/[category]/index.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" context="module">
+	import { Leaderboard, LeaderboardCategory } from "@libs/leaderboard";
+	import type { Load } from "@sveltejs/kit";
+
+	export const load: Load = async ({ page }) => {
+		return { props: { category: page.params.category } };
+	};
+</script>
+
+<script lang="ts">
+	interface $$Props {
+		category: LeaderboardCategory;
+	}
+
+	export let category: LeaderboardCategory;
+</script>
+
+<Leaderboard {category} />
