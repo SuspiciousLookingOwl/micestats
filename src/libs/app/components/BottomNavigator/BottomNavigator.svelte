10072ab6ba08896833de7ba034d380932b2df088
VincentJonathan <vinjongm@gmail.com>
2022-01-06 11:38:47 +0700

    smol refactor

--- src/libs/app/components/BottomNavigator/BottomNavigator.svelte
+++ src/libs/app/components/BottomNavigator/BottomNavigator.svelte
@@ -1,22 +1,41 @@
 <script lang="ts">
-	import { Icon } from "@components";
+	import { Icon, type Icons } from "@components";
+
+	interface Item {
+		name: Icons;
+		href: string;
+		large?: boolean;
+	}
+
+	const items: Item[] = [
+		{
+			name: "home",
+			href: "/",
+		},
+		{
+			name: "leaderboard",
+			href: "/leaderboards",
+			large: true,
+		},
+		{
+			name: "search",
+			href: "/search",
+		},
+		{
+			name: "menu",
+			href: "/#",
+		},
+	];
 </script>
 
 <div class="h-14" />
 
 <div class="bottom-nav">
-	<a class="item" href="/">
-		<Icon name="home" size="lg" class="fill-current mx-auto" />
-	</a>
-	<a class="item" href="/leaderboards">
-		<Icon name="leaderboard" size="2xl" class="fill-current mx-auto" />
-	</a>
-	<a class="item" href="/search">
-		<Icon name="search" size="lg" class="fill-current mx-auto" />
-	</a>
-	<div class="item" on:click={() => alert("Coming Soon")}>
-		<Icon name="menu" size="lg" class="fill-current mx-auto" />
-	</div>
+	{#each items as { name, large, href }}
+		<a class="item" {href}>
+			<Icon {name} size={large ? "2xl" : "lg"} class="fill-current mx-auto" />
+		</a>
+	{/each}
 </div>
 
 <style lang="postcss">
