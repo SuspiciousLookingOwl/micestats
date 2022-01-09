4123e4ec39448ef2c68111c9230fad728e9346aa
VincentJonathan <vinjongm@gmail.com>
2022-01-09 10:45:06 +0700

    add title

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -2,6 +2,10 @@
 	import { _ } from "svelte-i18n";
 </script>
 
+<svelte:head>
+	<title>Oops | Cheeseformice</title>
+</svelte:head>
+
 <div class="flex flex-col justify-center my-32">
 	<div class="space-y-8">
 		<div class="font-brand text-6xl">{$_("error.title")}</div>
