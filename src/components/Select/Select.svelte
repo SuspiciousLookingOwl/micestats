1357be211fa7cc5087d5aff11dd9e6aa355ceeac
VincentJonathan <vinjongm@gmail.com>
2022-01-11 22:53:19 +0700

    add on:change event

--- src/components/Select/Select.svelte
+++ src/components/Select/Select.svelte
@@ -8,13 +8,28 @@
 </script>
 
 <script lang="ts">
+	import { createEventDispatcher } from "svelte";
 	import CustomSelect from "./CustomSelect.svelte";
 	import NativeSelect from "./NativeSelect.svelte";
+
+	interface Events {
+		change: Option | null;
+	}
+
+	interface $$Events {
+		change: CustomEvent<Events["change"]>;
+	}
+
+	const dispatch = createEventDispatcher<Events>();
 
 	//#region props
 	export let options: Option[] = [];
 	export let variant: SelectVariant = "md";
 	export let value: Option | null = options[0] || null;
+	//#endregion
+
+	//#region event handlers
+	$: dispatch("change", value);
 	//#endregion
 </script>
 
