cda8947bef52edeefcaadfb11f5726bae77693e9
VincentJonathan <vinjongm@gmail.com>
2022-02-05 13:13:44 +0700

    add heatmap component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./AsyncLoader";
 export * from "./Card";
 export * from "./Carousel";
+export * from "./Heatmap";
 export * from "./Icon";
 export * from "./Image";
 export * from "./Input";
