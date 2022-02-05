4cd53cd0b5f109de1a3e93142286082b74f1521d
VincentJonathan <vinjongm@gmail.com>
2022-02-05 12:47:40 +0700

    add item active class

--- src/libs/app/components/BottomNavigator/BottomNavigator.svelte
+++ src/libs/app/components/BottomNavigator/BottomNavigator.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
+	import { page } from "$app/stores";
 	import { Icon, type Icons } from "@components";
+	import classNames from "classnames";
 
 	interface Item {
 		name: Icons;
 		href: string;
 		large?: boolean;
+		exact?: boolean;
 	}
 
 	const items: Item[] = [
 		{
 			name: "home",
 			href: "/",
+			exact: true,
 		},
 		{
 			name: "leaderboard",
@@ -31,8 +35,13 @@
 <div class="h-14" />
 
 <div class="bottom-nav">
-	{#each items as { name, large, href }}
-		<a class="item" {href}>
+	{#each items as { name, large, href, exact }}
+		<a
+			class={classNames("item", {
+				"item-active": exact ? $page.path === href : $page.path.startsWith(href),
+			})}
+			{href}
+		>
 			<Icon {name} size={large ? "2xl" : "lg"} class="fill-current mx-auto" />
 		</a>
 	{/each}
@@ -47,6 +56,10 @@
 	}
 
 	.item {
-		@apply flex w-full h-full items-center justify-center;
+		@apply flex w-full h-full items-center justify-center transition-colors text-neutral-300;
+	}
+
+	.item-active {
+		@apply bg-gray-800 text-neutral-50;
 	}
 </style>
